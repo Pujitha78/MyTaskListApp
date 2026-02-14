405367e1cf1a307c30c2d1deaa414e822de6f218
Kadiyala Pujitha <104677959+Pujitha78@users.noreply.github.com>
2026-02-14 14:44:51 +0530

    Initial Commit

---
+++ MyTaskListApp/MyTaskListAppApp.swift
@@ -0,0 +1,21 @@
+//
+//  MyTaskListAppApp.swift
+//  MyTaskListApp
+//
+//  Created by Pujitha Kadiyala on 2/14/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MyTaskListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
